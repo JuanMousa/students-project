fd87c717e71ba45ea9bd6b919706c9438d4732bc
JaunMousa <juan.mousa@powercoders.org>
2024-04-26 14:00:54 +0200

    last version

--- students.js
+++ students.js
@@ -1,39 +1,25 @@
-const studentList = [{
-    name : "Olha",
-    skill : ["HTML", "CSS", "JS"],
-    hobby : ["programing", "reading"],
-    photo: "img/Olha-1.jpg"    
-}, {
-    name : "Juan",
-    skills : ["HTML", "CSS", "JS", "Three.js"],
-    hobby : ["programing", "reading"],
-    photo: "img/Juan-1.jpg.jpg"
-}, {
-    name : "Shakir",
-    skills : ["HTML", "CSS", "JS", "SQL", "C#", "JAVA"],
-    hobby : ["programing", "reading"],
-    photo: "img/Shakir-1.jpg.jpg"
-}, {
-    name : "Frank",
-    skills : ["HTML", "CSS", "JS", "Python"],
-    hobby : ["programing", "reading"],
-    photo: "img/Frank-1.jpg.jpg"
-}, {
-    name : "Yuri",
-    skills : ["HTML", "CSS", "JS"],
-    hobby : ["programing", "reading"],
-    photo: "img/Frank-1.jpg.jpg"
-    
-}, {
-    name : "Yuri",
-    skills : ["HTML", "CSS", "JS"],
-    hobby : ["programing", "reading"],
-    photo: "img/Yuri-1.jpg.jpg.jpg"
-}, {
-    name : "Enes",
-    skills : ["HTML", "CSS", "JS"],
-    hobby : ["programing", "reading"],
-    photo: "img/Enes-1.jpg.jpg"
-}
+const studentList = [
+  {
+    name: "Olha",
+    skill: ["HTML", "CSS", "JS"],
+    hobby: ["programming", "reading"],
+    photo: "img/Olha-1.jpg",
+  },
+  {
+    name: "juan",
+    skill: ["HTML", "CSS", "JS"],
+    hobby: ["programming", "reading"],
+    photo: "img/Juan-1.jpg",
+  },
+  {
+    name: "Yuri",
+    skill: ["HTML", "CSS", "JS"],
+    hobby: ["programming", "reading"],
+    photo: "img/Yuri-1.jpg",
+  }, {
+    name: "Shakir",
+    skill: ["HTML", "CSS", "JS"],
+    hobby: ["programming", "reading"],
+    photo: "img/Shakir-1.jpg",
+  }
 ];
-

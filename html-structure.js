17fdb0603a343c4d6c85f53af54f9449e1bce2cd
olhalesyk <olha.lesyk@powercoders.org>
2024-04-26 22:29:53 +0200

    Improved styles

--- html-structure.js
+++ html-structure.js
@@ -3,7 +3,7 @@ function appendStudentsToUl(studentList, studentElement) {
   studentList.forEach((student, index) => {
     
     const studentCard = document.createElement("div");
-    studentCard.classList.add(`student-${index}`, `col-3`); // Добавляем класс "student-N", где N - индекс студента в массиве
+    studentCard.classList.add(`student-${index}`, `student-card`, `col-lg-3`, `col-md-4`, `col-6`,`m-30`); // Добавляем класс "student-N", где N - индекс студента в массиве
 
     const photo = document.createElement("img");
     photo.src = student.photo;
@@ -11,32 +11,30 @@ function appendStudentsToUl(studentList, studentElement) {
     photo.classList.add("student-photo"); // Добавляем класс "student-photo" к изображению
     studentCard.appendChild(photo);
 
+    const studentInfo = document.createElement("div");
+    studentInfo.classList.add("student-info", "text-center"); // Добавляем класс "student-name" к элементу <p> с именем
+    studentCard.appendChild(studentInfo);
+
     const name = document.createElement("h2");
     name.textContent = `${student.name}`;
     name.classList.add("student-name"); // Добавляем класс "student-name" к элементу <p> с именем
-    studentCard.appendChild(name);
+    studentInfo.appendChild(name);
     
     const skillsList = document.createElement("ul");
-    skillsList.classList.add("student-skills", "list-group", "list-group-horizontal-sm"); // Добавляем класс "student-skills" к элементу <p> с навыками
-    studentCard.appendChild(skillsList);
+    skillsList.classList.add("student-skills",); // Добавляем класс "student-skills" к элементу <p> с навыками
+    studentInfo.appendChild(skillsList);
     
     student.skills.forEach((skillItem) =>{
         const skillElement = document.createElement("li");
-        skillElement.classList.add("skills-item","list-group-item");
+        skillElement.classList.add("skills-item");
         skillElement.textContent = skillItem;
         skillsList.appendChild(skillElement);
     });
 
-    const hobbiesList = document.createElement("ul");
-    hobbiesList.classList.add("student-hobbies", "list-group", "list-group-horizontal-sm");
-    studentCard.appendChild(hobbiesList);
-
-    student.hobby.forEach((hobbyItem) =>{
-        const hobbyElement = document.createElement("li");
-        hobbyElement.classList.add("hobbies-item","list-group-item");
-        hobbyElement.textContent = hobbyItem;
-        hobbiesList.appendChild(hobbyElement);
-    });
+    const hobbiesElement = document.createElement("p");
+    hobbiesElement.textContent = `Hobbies: ${student.hobby.join(", ")}`;
+    hobbiesElement.classList.add("student-hobbies"); // Добавляем класс "student-hobbies" к элементу <p> с хобби
+    studentInfo.appendChild(hobbiesElement);
     
     studentElement.appendChild(studentCard);
   });
--- students.js
+++ students.js
@@ -1,33 +1,33 @@
 const studentList = [{
-  name: "Olha",
-  skills: ["HTML", "CSS", "JS", "Three.js"], //studentList[0].skills[0]
-  hobby: ["programing", "reading"],
-  photo: "img/Olha-1.jpg"
-}, {
   name: "Shakir",
   skills: ["HTML", "CSS", "JS", "SQL", "C#", "JAVA"],
-  hobby: ["programing", "reading"],
+  hobby: [" Programming", "Reading"],
   photo: "img/Shakir-1.jpg"
+}, {
+  name: "Olha",
+  skills: ["HTML", "CSS", "JS", "Three.js"], //studentList[0].skills[0]
+  hobby: [" Programming", "Reading", "Jogging"],
+  photo: "img/Olha-1.jpg"
 }, {
   name: "Frank",
   skills: ["HTML", "CSS", "JS", "Python"],
-  hobby: ["programing", "reading"],
+  hobby: [" Programming", "Reading"],
   photo: "img/Frank-1.jpg"
 }, {
   name: "Enes",
   skills: ["HTML", "CSS", "JS"],
-  hobby: ["programing", "reading"],
+  hobby: [" Programming", "Reading", "Jogging"],
   photo: "img/Enes-1.jpg"
 
 }, {
-  name: "Yuri",
+  name: "Yurij",
   skills: ["HTML", "CSS", "JS"],
-  hobby: ["programing", "reading"],
+  hobby: [" Programming", "Reading"],
   photo: "img/Yuri-1.jpg"
 }, {
   name: "Kashif",
   skills: ["HTML", "CSS", "JS", "REACT", "NEXT.JS"],
-  hobby: ["programing", "reading", "travelling"],
+  hobby: [" Programming", "Reading", "Travelling"],
   photo: "img/Kashif-1.jpg"
 }
 ];

053ca795d769decd513117e1da7871c9d856bc47
JaunMousa <juan.mousa@powercoders.org>
2024-04-26 11:25:03 +0200

    the hole Object

--- students.js
+++ students.js
@@ -3,5 +3,37 @@ const studentList = [{
     skill : ["HTML", "CSS", "JS"],
     hobby : ["programing", "reading"],
     photo: "img/Olha-1.jpg"    
-},
-]
+}, {
+    name : "Juan",
+    skills : ["HTML", "CSS", "JS", "Three.js"],
+    hobby : ["programing", "reading"],
+    photo: "img/Juan-1.jpg.jpg"
+}, {
+    name : "Shakir",
+    skills : ["HTML", "CSS", "JS", "SQL", "C#", "JAVA"],
+    hobby : ["programing", "reading"],
+    photo: "img/Shakir-1.jpg.jpg"
+}, {
+    name : "Frank",
+    skills : ["HTML", "CSS", "JS", "Python"],
+    hobby : ["programing", "reading"],
+    photo: "img/Frank-1.jpg.jpg"
+}, {
+    name : "Yuri",
+    skills : ["HTML", "CSS", "JS"],
+    hobby : ["programing", "reading"],
+    photo: "img/Frank-1.jpg.jpg"
+    
+}, {
+    name : "Yuri",
+    skills : ["HTML", "CSS", "JS"],
+    hobby : ["programing", "reading"],
+    photo: "img/Yuri-1.jpg.jpg.jpg"
+}, {
+    name : "Enes",
+    skills : ["HTML", "CSS", "JS"],
+    hobby : ["programing", "reading"],
+    photo: "img/Enes-1.jpg.jpg"
+}
+];
+
